ea98044d2af472c8e3425703a1af542733490264
robertnbritton <robertnbritton@gmail.com>
2021-09-21 12:22:11 -0600

    added dev variables, added baseURL and header to services

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,7 @@
 export const gitHub = "https://github.com/drachen2299/bug-catcher";
-export const baseURL = "";
+export const baseURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/bugs`;
+export const config = {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+    },
+  };
