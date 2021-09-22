940038a1eed475750fa2ae688215860fed5c013a
robertnbritton <robertnbritton@gmail.com>
2021-09-22 16:17:45 -0600

    form is now adding bugs to the API

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,12 +5,11 @@ import { baseURL, config } from "../services";
 export default function Form(props) {
     const [bug, setBug] = useState("");
     const [species, setSpecies] = useState("");
-    const [schmidtIndex, setSchmidtIndex] = useState(0);
+    const [schmidtPainIndex, setSchmidtPainIndex] = useState(0);
     const [imageURL, setImageUrl] = useState("");
     const [information, setInformation] = useState("");
     const [fieldReference, setFieldReference] = useState("");
     const [imageReference, setImageReference] = useState("");
-    const [schmidtPainIndexFull, setSchmidtPainIndexFull] = useState("");
    
 
     const handleSubmit = async (e) => {
@@ -18,12 +17,11 @@ export default function Form(props) {
         const newBug = {
             bug,
             species,
-            schmidtIndex,
+            schmidtPainIndex,
             imageURL,
             information,
             fieldReference,
             imageReference,
-            schmidtPainIndexFull,
         }
         
             await axios.post(baseURL, { fields: newBug }, config);
@@ -34,8 +32,7 @@ export default function Form(props) {
         <form className="form" onSubmit={handleSubmit}>
             <input type="text" placeholder="Bug Name" required value={bug} onChange={(e) => setBug(e.target.value)}/>
             <input type="text" placeholder="Species" required value={species} onChange={(e) => setSpecies(e.target.value)}/>
-            <input type="number" placeholder="Schmidt Index" required value={schmidtIndex} onChange={(e) => setSchmidtIndex(e.target.valueAsNumber)}/>
-            <input type="text" placeholder="Schmidt Index Info" required value={schmidtPainIndexFull} onChange={(e) => setSchmidtPainIndexFull(e.target.value)}></input>
+            <input type="number" placeholder="Schmidt Index" required value={schmidtPainIndex} onChange={(e) => setSchmidtPainIndex(e.target.valueAsNumber)}/>
             <input type="text" placeholder="Image URl" required value={imageURL} onChange={(e) => setImageUrl(e.target.value)}/>
             <input type="text" placeholder="Information" required value={information} onChange={(e) => setInformation(e.target.value)}></input>
             <input type="text" placeholder="Info Reference" required value={fieldReference} onChange={(e) => setFieldReference(e.target.value)}></input>
