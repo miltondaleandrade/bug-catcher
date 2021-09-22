d26bfbb9d98fb929a32c36a16b44d743dccc21cd
robertnbritton <robertnbritton@gmail.com>
2021-09-22 15:26:08 -0600

    tried getting my form to post but unsuccessfully getting error 422

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
-import { useHistory, useParams } from "react-router";
+import { useState } from "react";
 import { baseURL, config } from "../services";
 
 export default function Form(props) {
@@ -9,45 +8,38 @@ export default function Form(props) {
     const [schmidtIndex, setSchmidtIndex] = useState(0);
     const [imageURL, setImageUrl] = useState("");
     const [information, setInformation] = useState("");
-    const history = useHistory();
-    const params = useParams();
-
-    // useEffect(() => {
-    //     if (params.id) {
-    //         const Bugs = props.bugs.find((bug) => bug.id === params.id);
-    //         if (bug) {
-    //             setBug(bug.fields.bug);
-    //             setSpecies(bug.fields.species);
-    //             setSchmidtIndex(bug.fields.schmidtIndex);
-    //             setImageUrl(bug.fields.imageURL);
-    //             setInformation(bug.fields.information);
-    //         } 
-    //     }
-    // }, [params.id, props.bugs]);
+    const [fieldReference, setFieldReference] = useState("");
+    const [imageReference, setImageReference] = useState("");
+    const [schmidtPainIndexFull, setSchmidtPainIndexFull] = useState("");
+   
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const newBug = {
-            fields: {
             bug,
             species,
             schmidtIndex,
             imageURL,
             information,
-            }
+            fieldReference,
+            imageReference,
+            schmidtPainIndexFull,
         }
         
             await axios.post(baseURL, { fields: newBug }, config);
-        
+        }
 
-    }
+    
     return(
         <form className="form" onSubmit={handleSubmit}>
             <input type="text" placeholder="Bug Name" required value={bug} onChange={(e) => setBug(e.target.value)}/>
             <input type="text" placeholder="Species" required value={species} onChange={(e) => setSpecies(e.target.value)}/>
-            <input type="text" placeholder="Schmidt Index (Optional)" value={schmidtIndex} onChange={(e) => setSchmidtIndex(e.target.valueAsNumber)}/>
+            <input type="number" placeholder="Schmidt Index" required value={schmidtIndex} onChange={(e) => setSchmidtIndex(e.target.valueAsNumber)}/>
+            <input type="text" placeholder="Schmidt Index Info" required value={schmidtPainIndexFull} onChange={(e) => setSchmidtPainIndexFull(e.target.value)}></input>
             <input type="text" placeholder="Image URl" required value={imageURL} onChange={(e) => setImageUrl(e.target.value)}/>
-            <textarea type="text" cols="30" rows="10" placeholder="Information" required value={information} onChange={(e) => setInformation(e.target.value)}></textarea>
+            <input type="text" placeholder="Information" required value={information} onChange={(e) => setInformation(e.target.value)}></input>
+            <input type="text" placeholder="Info Reference" required value={fieldReference} onChange={(e) => setFieldReference(e.target.value)}></input>
+            <input type="text" placeholder="Image Reference" required value={imageReference} onChange={(e) => setImageReference(e.target.value)}></input>
             <button type="submit">Create!</button>
         </form>
     )
--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  
     <Router>
     <App />
     </Router>
     
-  </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
 
